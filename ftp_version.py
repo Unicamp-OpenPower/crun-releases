f2f5fe87efdefca09fbad786523e1b43d42496f6
Guilherme Dobins <70792082+GuilhermeDobins@users.noreply.github.com>
2022-05-12 00:27:15 -0300

    Fixed code for versions

--- ftp_version.py
+++ ftp_version.py
@@ -4,7 +4,7 @@ html = str(
     requests.get('https://github.com/containers/crun/releases/latest')
     .content)
 index = html.find('Release ')
-github_version = html[index + 8:index + 12]
+github_version = html[index + 8:index + 13]
 file = open('github_version.txt', 'w')
 file.writelines(github_version)
 file.close()
@@ -15,7 +15,7 @@ html = str(
         'https://oplab9.parqtec.unicamp.br/pub/ppc64el/crun/latest'
     ).content)
 index = html.rfind('crun-')
-ftp_version = html[index + 5:index + 9]
+ftp_version = html[index + 5:index + 10]
 file = open('ftp_version.txt', 'w')
 file.writelines(ftp_version)
 file.close()
